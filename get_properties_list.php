53a649c2e9ddaa670c0cd3f8f17c7dedae29c991
Ammar Zainh <arammarm@gmail.com>
2025-12-02 18:27:15 +0530

    Updst 2 pull test

--- get_properties_list.php
+++ get_properties_list.php
@@ -10,7 +10,7 @@ APIHelper::RemoveParamIfZero();
 APIHelper::setAccuracyParams();
 
 $test = APIHelper::GetParam('test');
-$distanceMax = APIHelper::GetParam('distance_max');
+$distanceMax = APIHelper::GetParam('distance_max'); 
 $userLat = APIHelper::GetParam('user_lat'); 
 $userLng = APIHelper::GetParam('user_lng');
 $allWholesale = APIHelper::GetParam('all_wholesale');
